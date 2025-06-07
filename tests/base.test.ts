112517983ece4df6eca1f33f774059a0f93bcb40
JoMath363 <JoMath363@users.noreply.github.com>
2025-06-07 10:40:18 -0300

    config jest

---
+++ tests/base.test.ts
@@ -0,0 +1,11 @@
+describe('Example test', () => {
+  it('should pass', () => {
+    expect(1 + 1).toBe(2);
+  });
+});
+
+describe('Example test', () => {
+  it('should not pass', () => {
+    expect(1 + 1).toBe(3);
+  });
+});
